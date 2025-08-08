506c0774d22623c28b431c5db677d66c186539de
Hassnae769 <hasnaidelkadi@gmail.com>
2025-08-08 17:13:26 +0100

    Update blocks.js

--- packages/cli/src/template/src/blocks.js
+++ packages/cli/src/template/src/blocks.js
@@ -1,13 +1,6 @@
 export default (editor, opts = {}) => {
   const bm = editor.BlockManager;
 
-  bm.add('merge-field-firstname', {
-    label: 'Prénom Contact',
-    category: 'Champs de fusion',
-    content: {
-      type: 'text',
-      content: '{{Contact.FirstName}}',
-    },
-  });
+  
 };
 
